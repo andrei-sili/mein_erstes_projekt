594127cceea93573b4a9d172e112b6fe5f8a40ce
Andrei_Sili <andrei.sili1985@gmail.com>
2024-11-26 20:58:22 +0100

    Teil 3, Aufgabe 7: Rückwärts iterieren

---
+++ teil3_aufgabe_7.py
@@ -0,0 +1,5 @@
+"""Aufgabe 7: Rückwärts iterieren"""
+
+if __name__ == "__main__":
+    print("*" * 15, "Aufgabe 7: Rückwärts iterieren", "*" * 15)
+    print(*[i for i in range(10, 0, -1)])  # list generator
