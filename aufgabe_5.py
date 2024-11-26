e50b25759756d19d58e9639ce1809fddd002e97b
Andrei_Sili <andrei.sili1985@gmail.com>
2024-11-26 11:13:35 +0100

    Aufgabenblatt Teil 2, Aufgabe 5: Benutzereingabe

--- aufgabe_5.py
+++ aufgabe_5.py
@@ -1,1 +1,14 @@
+"""Aufgabe 5: Benutzereingabe"""
 
+
+def begrüßung():
+    name = input("Ihre Namen: ")
+    beruf = input("Ihre Beruf: ")
+    alter = input("Ihre Alter: ")
+    print("\n--- Begrüßungsnachricht ---")
+    print(f"Hallo {name}!\nDu bist {alter} Jahre alt und arbeitest als {beruf}.\nSchön, dich kennenzulernen!")
+
+
+if __name__ == "__main__":
+    print("*" * 15, "Aufgabe 5: Benutzereingabe", "*" * 15)
+    begrüßung()
