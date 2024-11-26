cc6379ae8f2eddf3c66e7569201a93e00529c861
Andrei_Sili <andrei.sili1985@gmail.com>
2024-11-26 17:37:54 +0100

    Teil 3, Aufgabe 3: Passworteingabe

---
+++ teil3_aufgabe_3.py
@@ -0,0 +1,6 @@
+"""Aufgabe 3: Passworteingabe"""
+from aufgabe_2 import passwort_check
+
+if __name__ == "__main__":
+    print("*" * 15, "Aufgabe 3: Passworteingabe", "*" * 15)
+    passwort_check()  # richtige passwort ist "passwort"
