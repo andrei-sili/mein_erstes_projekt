b80d2edc6a7279452b66835c52a78c771194bd97
Andrei_Sili <andrei.sili1985@gmail.com>
2024-11-26 11:12:43 +0100

    Aufgabenblatt Teil 2, Aufgabe 4: Verwendung von Escape-Zeichen

--- aufgabe_4.py
+++ aufgabe_4.py
@@ -1,1 +1,11 @@
+"""Aufgabe 4: Verwendung von Escape-Zeichen"""
 
+
+def escape():
+    print("Ta\tbu\tla\ttor")
+    print("Zeilen-\numbruch")
+
+
+if __name__ == "__main__":
+    print("*" * 15, "Aufgabe 4: Verwendung von Escape-Zeichen", "*" * 15)
+    escape()
