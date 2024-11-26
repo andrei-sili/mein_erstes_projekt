388bd312ca4f9554de9637f8f187ca4e7209ab17
Andrei_Sili <andrei.sili1985@gmail.com>
2024-11-26 11:12:11 +0100

    Aufgabenblatt Teil 2, Aufgabe 3: Grundlegende Print-Ausgabe

--- aufgabe_3.py
+++ aufgabe_3.py
@@ -1,1 +1,16 @@
+"""Aufgabe 3: Grundlegende Print-Ausgabe"""
 
+
+def user_data():
+    namen = "Andrei Sili"
+    ausbildung = "Fachinformatiker"
+    zitat = "Auch der weiteste Weg beginnt mit einem ersten Schritt"
+    print(f"Meine Name ist {namen}, ich bin {ausbildung} und meine lieblings Zitat ist: '{zitat}'.")
+    print("Meine Name ist {}, ich bin {} und meine lieblings Zitat ist: '{}'.".format(namen, ausbildung, zitat))
+
+
+
+
+if __name__ == "__main__":
+    print("*" * 15, "Aufgabe 3: Grundlegende Print-Ausgabe", "*" * 15)
+    user_data()
