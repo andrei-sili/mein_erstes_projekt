44ea5603b6e3ce3036e21fd6de25b601c8a7aa9b
Andrei_Sili <andrei.sili1985@gmail.com>
2024-11-27 09:25:57 +0100

    Teil 3, Aufgabe 11: Iterieren über Listen

---
+++ teil3_aufgabe_11.py
@@ -0,0 +1,12 @@
+"""Aufgabe 11: Iterieren über Listen"""
+
+
+def iter_list():
+    l = [10, 20, 30, 40, 50]
+    for i in l:
+        print(i)
+
+
+if __name__ == "__main__":
+    print("*" * 15, "Aufgabe 11: Iterieren über Listen", "*" * 15)
+    iter_list()
