82b81e81dea0ee68e5449eb95bc4e624269aeaa4
Andrei_Sili <andrei.sili1985@gmail.com>
2024-11-27 09:46:56 +0100

    Teil 3, Aufgabe 13: Iteration über einen String

---
+++ teil3_aufgabe_13.py
@@ -0,0 +1,12 @@
+"""Aufgabe 13: Iteration über einen String"""
+
+
+def iter_string():
+    text = "Aufgabe 13"
+    for buchstaben in text:
+        print(buchstaben)
+
+
+if __name__ == "__main__":
+    print("*" * 15, "Aufgabe 13: Iteration über einen String", "*" * 15)
+    iter_string()
