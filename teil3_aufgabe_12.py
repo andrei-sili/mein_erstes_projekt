7067f1de2031721c43947d14ab92717642505ac2
Andrei_Sili <andrei.sili1985@gmail.com>
2024-11-27 09:40:01 +0100

    Teil 3, Aufgabe 12: Summieren von Elementen

---
+++ teil3_aufgabe_12.py
@@ -0,0 +1,8 @@
+"""Aufgabe 12: Summieren von Elementen"""
+
+num_list = [2, 3, 567, 5.6, -67]
+
+if __name__ == "__main__":
+    print("*" * 15, "Aufgabe 12: Summieren von Elementen", "*" * 15)
+    print(f"Summe aller Elemente in num_list = {sum(num_list)}")
+    
